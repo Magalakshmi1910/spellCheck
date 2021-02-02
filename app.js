cb82a90ef9bafd8b4b88dfae19151bc961cd3dfd
Magalakshmi1910 <72559143+Magalakshmi1910@users.noreply.github.com>
2021-02-02 11:29:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
 readline = require('readline');
 
+//check if user has entered the file name
 if(process.argv.length !== 3)
 {
   console.log("enter text file name as arguement");
   return 1;
 }
+
+//use readline to read the text file line by line 
 var rd = readline.createInterface({
     input: fs.createReadStream(process.argv[2])
 });
@@ -13,16 +16,24 @@ var rd = readline.createInterface({
 rd.on('line', function(line) {
   var wordArray = [];
   var misspelled = [];
+  
+  //line returns a single character. for example- "the" is returned as 't','h','e'
+  //convert each character to word and push the words into array using split
   wordArray = line.split(/[\s,.]+/);
-
+  
+  //read dictionary file
   fs.readFile('dictionary.txt','utf8',function(err,data){
     var count =  0;
      var dictionary = data.split(/\r?\n/);
 
      if (err) throw  err;
+    //loop through each words in the array to be checked for spelling
      for(var i=0;i<wordArray.length;i++)
      {
+       //indexOf searches for an exact match and if not found returns -1
         if(dictionary.indexOf(wordArray[i].toLowerCase())<0){
+          
+          //count the misspelled words and psuh the words into a new array
           count++;
           misspelled.push(wordArray[i]);
         }
@@ -31,6 +42,5 @@ rd.on('line', function(line) {
     for(var i=0;i<misspelled.length;i++){
       console.log(misspelled[i]);
     }
-
 });
 });
